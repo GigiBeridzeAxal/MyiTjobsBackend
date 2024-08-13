f7e7cecb3eab476b356f630456a37287b1a84769
GigiBeridzeAxal <145029127+GigiBeridzeAxal@users.noreply.github.com>
2024-08-13 23:43:22 +0400

    Commit

--- controllers/NodeMailer.js
+++ controllers/NodeMailer.js
@@ -30,7 +30,7 @@ const sendmail = async(req,res) => {
       async function main() {
         // send mail with defined transport object
         const info = await transporter.sendMail({
-          from: '"My-ItJobs "', // sender address
+          from: 'My-ItJobs', // sender address
           to: email, // list of receivers
           subject: name, // Subject line
           text: "Hello world?", // plain text body
